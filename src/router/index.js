9d56516c56a83c98e809ac257f81630153b8c50e
sgoyanka <sgoyanka@grepruby.com>
2017-08-08 19:41:18 +0530

    Routes file added

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue/dist/vue.js'
+import Router from 'vue-router'
+import FormComponent from '../form.vue'
+import FormData from '../formData.vue'
+import App from '../app.vue'
+
+Vue.use(Router)
+var data = App.data();
+export default new Router({
+
+  routes: [
+    {
+      path: '/',
+      component: FormData,
+      props : true
+    },
+    {
+      path: '/create',
+      component: FormComponent,
+      props : true
+    },
+  ]
+})
