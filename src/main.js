de7a890cb2fd7c618962485ff0db46aa5b1b983f
sgoyanka <sgoyanka@grepruby.com>
2017-08-08 19:44:32 +0530

    Main file added routes and started it

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue/dist/vue.js'
 import App from './app.vue'
+import router from './router'
 
 Vue.component('app', App)
-
+  
 new Vue({
-  el: '#divid'
+  el: '#divid',
+  router,
+  template: '<App/>',
+  components: { App }
  
 })
