9f4ab6f45b531aee84ef2532de0b4c2a0f56f882
sgoyanka <sgoyanka@grepruby.com>
2017-08-09 17:22:31 +0530

    Removed all data from app.vue

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,5 @@
 <template>
-	<router-view v-bind:users="users" v-bind:remove="remove" v-bind:addData="addData" v-bind:editData="editData" v-bind:newData="newData" v-bind:user="user"></router-view>		
+	<router-view ></router-view>		
 </template>
 
 <script>
@@ -7,53 +7,10 @@
 	import FormData from './formData.vue'
 	
 	export default {
-		data () {
-			return {
-				loginType: true,
-      	users : [{
-          id:0,
-          name : "shanu",
-          email : "abc@w",
-          password : "Dajshb@1233"
-        }],
-			  id : 1,
-			  user : {},
-			  index : 0
-    	} 
-		},
+		
 	 	components : {
   		FormComponent,
   		FormData
-  	},
-  	methods : {
-  		newData (user) {
-  			if(!user.hasOwnProperty("id")) {
-  		  this.users.push({
-          id : this.id,
-          name : user.name,
-          email : user.email,
-          password : user.password
-        });
-        this.id++;
-  			} else {
-  				this.users[this.index] = user;
-  			}
-  			console.log(this.users);
-  			window.location = "/#/"
-  		},
-  		remove(index) {
-  			this.users.splice(index,1);
-  		},
-  		addData() {
-        this.user = {};
-  			window.location = "/#/create";
-  		},
-  		editData(index) {
-  			this.index = index;
-  			this.user = this.users[index];
-  			window.location = "/#/users/" + this.user.id; 
-  		}
-  		
   	}
 	}
 	  
